d5fa8221bd592279f2b75ff0398abdf9c2407bcb
Margaret Diaz <margaret.diaz05@myhunter.cuny.edu>
2023-04-16 01:39:03 -0400

    got the map to show up

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
-import { useEffect, useState } from "react";
+import {useRef, useEffect, useState } from "react";
 import "./App.css";
 import logo from "./mlh-prep.png";
 import AutoComp from "./components/AutoComp";
 import usePlacesAutocomplete from "use-places-autocomplete";
 import { useLoadScript } from "@react-google-maps/api";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
 
 function App() {
+  const mapContainerRef = useRef(null);
+  const [map, setMap] = useState(null);
+
   const [error, setError] = useState(null);
   const [isVarLoaded, setIsVarLoaded] = useState(false);
   const [city, setCity] = useState("New York City");
   const [results, setResults] = useState(null);
-  const { isLoaded } = useLoadScript({
+  const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY,
     libraries: ["places"],
   });
+
+  const myIcon = L.icon({
+    iconUrl: './components/icon.png',
+    iconSize: [50, 50],
+    iconAnchor: [25, 50],
+    popupAnchor: [0, -50]
+  });
+  
+  //  const marker = L.marker([51.5, -0.09], { icon: myIcon }).addTo(map);
+  // marker.bindPopup("<b>Hello world!</b><br>I am a custom marker.");
 
   useEffect(() => {
     fetch(
@@ -40,6 +55,39 @@ function App() {
       );
   }, [city]);
 
+  
+  useEffect(() => {
+    if (!isLoaded || !mapContainerRef.current) {
+      return;
+    }
+
+    const map = L.map(mapContainerRef.current).setView([51.505, -0.09], 13);
+
+    L.tileLayer(
+      "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+      {
+        attribution:
+          'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+          '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+          'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        tileSize: 512,
+        zoomOffset: -1,
+      }
+    ).addTo(map);
+
+    // Add the click event listener to the map
+    map.on("click", (e) => {
+    const { lat, lng } = e.latlng;
+    console.log(lat, lng);
+    // Use the lat and lng values to fetch weather data for the clicked location
+    // Add a marker to the clicked location
+    const marker = L.marker([lat, lng]).addTo(map);
+    });
+
+    setMap(map);
+  }, [isLoaded]);
+
   const cityHandler = (city) => {
     console.log("City set to:", city);
     setCity(city);
@@ -54,6 +102,15 @@ function App() {
         <div>
           <h2>Enter a city below 👇</h2>
           {isLoaded && <AutoComp cityHandler={cityHandler}></AutoComp>}
+          {loadError && <div>Failed to load map</div>}
+          {!loadError && !isLoaded && <div>Loading map...</div>}
+          {!loadError && isLoaded && (
+            <div
+              ref={mapContainerRef}
+              style={{ height: "500px", width: "100%" }}
+              >
+          </div>
+          )}
           <div className="Results">
             {!isVarLoaded && <h2>Loading...</h2>}
             {console.log(results)}
